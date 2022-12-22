36106e60ace54832382a99c3cbd73d57e353e3b9
Joshua <joshua.jacobowitz@yahoo.de>
2022-12-22 23:44:41 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+declare const OpaqueIdentifier: unique symbol;
+type Opaque<T extends symbol> = { readonly [OpaqueIdentifier]: T };
+
+export type Nominal<T, U extends symbol> = T & Opaque<U>;
+
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
+export function nominal<T extends Opaque<any> = never>(
+  v: T extends infer V & Opaque<T[typeof OpaqueIdentifier]> ? V : never
+): NoInfer<T> {
+  return v as T;
+}
---
+++ test.ts
@@ -0,0 +1,55 @@
+import { expectType, TypeEqual } from "ts-expect";
+import { nominal, Nominal } from "./index";
+import assert from "assert";
+
+declare const FOO: unique symbol;
+type FooString = Nominal<string, typeof FOO>;
+const createFoo = nominal<FooString>;
+
+declare const BAR: unique symbol;
+type BarString = Nominal<string, typeof BAR>;
+const createBar = nominal<BarString>;
+
+function fooFn(_: FooString) {}
+function barFn(_: BarString) {}
+
+const fooString = createFoo("");
+const barString = createBar("");
+
+fooFn(fooString);
+barFn(barString);
+fooFn(createFoo(""));
+barFn(createBar(""));
+fooFn(nominal<FooString>(""));
+barFn(nominal<BarString>(""));
+
+// @ts-expect-error
+nominal<FooString>(1);
+// @ts-expect-error
+nominal<BarString>(1);
+// @ts-expect-error
+fooFn(barString);
+// @ts-expect-error
+barFn(fooString);
+// @ts-expect-error
+fooFn("");
+// @ts-expect-error
+barFn("");
+// @ts-expect-error
+fooFn(nominal(""));
+// @ts-expect-error
+barFn(nominal(""));
+// @ts-expect-error
+createFoo(1);
+// @ts-expect-error
+fooFn(barString);
+
+expectType<(_: string) => FooString>(nominal<FooString>);
+expectType<TypeEqual<typeof nominal<never>, (_: never) => never>>(true);
+expectType<TypeEqual<Parameters<typeof nominal>, never>>(true);
+
+assert.strictEqual(nominal<FooString>("foo"), "foo");
+
+type Foo = Nominal<{ foo: string }, typeof FOO>;
+const foo = { foo: "bar" };
+assert.strictEqual(nominal<Nominal<Foo, typeof FOO>>(foo), foo);

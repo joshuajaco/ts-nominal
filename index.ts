f67c02e25119dd7f67fd1c357c934c80c17079d1
Joshua Jacobowitz <joshua.jacobowitz@yahoo.de>
2022-12-23 04:26:13 +0100

    Rename internal types

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-declare const OpaqueIdentifier: unique symbol;
-type Opaque<T extends symbol> = { readonly [OpaqueIdentifier]: T };
+declare const BrandIdentifier: unique symbol;
+type Brand<T extends symbol> = { readonly [BrandIdentifier]: T };
 
 export type Nominal<T, U extends symbol> = 0 extends 1 & T
   ? never
-  : T & Opaque<U>;
+  : T & Brand<U>;
 
 type NoInfer<T> = [T][T extends any ? 0 : never];
 
-export function nominal<T extends Opaque<any> = never>(
+export function nominal<T extends Brand<any> = never>(
   value: 0 extends 1 & T
     ? never
-    : T extends infer V & Opaque<T[typeof OpaqueIdentifier]>
+    : T extends infer V & Brand<T[typeof BrandIdentifier]>
     ? V
     : never
 ): NoInfer<T> {
